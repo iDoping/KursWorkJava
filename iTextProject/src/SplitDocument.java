bc6022ece2441a1b6166ee23d82ebc335c0bd316
iDoping <43810683+iDoping@users.noreply.github.com>
2019-04-20 13:02:38 +0300

    Add files via upload

--- iTextProject/src/SplitDocument.java
+++ iTextProject/src/SplitDocument.java
@@ -1,34 +1,22 @@
+import com.itextpdf.awt.geom.AffineTransform;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SplitDocument {
 
-   // private static final String RESULT = "C:/Users/Reado/Desktop/Lukin.pdf";
-
         public static void main(String[] args) {
+
             List<InputStream> list = new ArrayList<InputStream>();
             try {
-                String inFile = "D:/Lukin.pdf".toLowerCase();
+                String inFile = "D:/Отчет по практике.pdf".toLowerCase();
                 System.out.println("Чтение " + inFile);
                 PdfReader reader = new PdfReader(inFile);
                 int n = reader.getNumberOfPages();
-
-                int rot;
-                PdfDictionary pageDict;
-                for (int i = 1; i <= n; i++) {
-                    rot = reader.getPageRotation(i);
-                    pageDict = reader.getPageN(i);
-                    pageDict.put(PdfName.ROTATE, new PdfNumber(rot + 90));
-                }
-                //PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(RESULT));
-                //stamper.close();
-               // reader.close();
-
                 System.out.println("Количество страниц : " + n);
                 int i = 0;
                 while (i < n) {
@@ -43,7 +31,6 @@ public class SplitDocument {
                     document.close();
                     writer.close();
                 }
-
                 OutputStream out = new FileOutputStream(new File("D:/result.pdf"));
                 doMerge(list, out);
             } catch (Exception e) {
@@ -51,8 +38,8 @@ public class SplitDocument {
             }
         }
 
-    public static void doMerge(List<InputStream> list, OutputStream outputStream)
-            throws DocumentException, IOException {
+    public static void doMerge(List<InputStream> list, OutputStream outputStream) throws DocumentException, IOException {
+
         Document document = new Document();
         PdfWriter writer = PdfWriter.getInstance(document, outputStream);
         document.open();
@@ -61,16 +48,19 @@ public class SplitDocument {
         for (InputStream in : list) {
             PdfReader reader = new PdfReader(in);
             for (int i = 1; i <= reader.getNumberOfPages(); i++) {
-                document.newPage();
-                //import the page from source pdf
                 PdfImportedPage page = writer.getImportedPage(reader, i);
-                //add the page to the destination pdf
-                cb.addTemplate(page, 0, 0);
+                document.setPageSize(new Rectangle(page.getHeight(), page.getWidth()));
+                document.newPage();
+                float min = Math.min(page.getHeight(), page.getWidth());
+                AffineTransform rotateInstance = AffineTransform.getRotateInstance(Math.PI/-2,min /1.9, min / 1.9);
+                double[] matrix = new double[6];
+                rotateInstance.getMatrix(matrix);
+                cb.addTemplate(page, (float) matrix[0], (float) matrix[1], (float) matrix[2],(float) matrix[3], (float) matrix[4], (float) matrix[5]);
             }
         }
-
         outputStream.flush();
         document.close();
         outputStream.close();
+
     }
 }
